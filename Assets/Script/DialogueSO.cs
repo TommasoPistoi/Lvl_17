233497ecb275e29ae81f05c123f9a5ceec00caa6
NoemiSossella <noemi.sossella@gmail.com>
2025-11-21 16:08:39 +0100

    DialogueSystem

---
+++ Assets/Script/DialogueSO.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewDialogue", menuName = "Dialogue")]
+public class DialogueSO : ScriptableObject
+{
+    public string[] lines;
+}
